026ee246403e43d47a5efd8178bfbd1d0b765c0b
Selma Elgabalawy <salmosha88@gmail.com>
2021-02-16 21:09:49 -0800

    stable code

--- employee_directory_app/src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=100&nat=us";
-
-export const API = {
-  getUsers: function () {
-    return axios.get(BASEURL);
-  },
-};
